c2a6112ae20ca2185d5d9c4ce2b67e484bd521a2
trapala85mx <rtrapala.dev.ds@gmail.com>
2023-08-26 12:11:35 -0600

    Docstring for decorators.py

--- crypto_info/utils/decorators.py
+++ crypto_info/utils/decorators.py
@@ -4,12 +4,21 @@ import functools
 # Externals
 
 
-def singleton(cls):
-    
+def singleton(cls) -> function:
+    """Class Decorator to apply Singleton
+
+    Returns:
+        function: function that will be executed instead of the one that is being decorated
+    """    
     _instances = {}
     
     @functools.wraps(cls)
     def wrapper(*args, **kwargs):
+        """Wraps that will search for the instance and returns it
+
+        Returns:
+            [type]: The class with its args / kwargs
+        """        
         if cls not in _instances:
             _instances[cls] = cls(*args, **kwargs)
         return _instances[cls]
